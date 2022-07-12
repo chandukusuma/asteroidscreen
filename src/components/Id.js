e02840ac2963d9a488cc0de5ddc2ded2765e3a7a
chandra shekar <kusuma_fw12_279@masai.school>
2022-07-12 15:02:39 +0530

    id page created

--- src/components/Id.js
+++ src/components/Id.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import axios from "axios";
+import {useNavigate} from "react-router-dom"
 
 function Id() {
 
+
+    const navigate = useNavigate()
 
     const [pick, setPick] = useState();
 
@@ -11,31 +14,38 @@ function Id() {
         .then((res) => {
 
             setPick(res.data.near_earth_objects);
-            console.log(pick)
+            console.log(res.data.near_earth_objects)
 
-            // const listItems = pick.map((e) => {
-            //     <li>{e.id}</li>
-            // })
 
         })
     }
 
+    // const listItems = pick.map((e) => {
+    //     <li>{e.id}</li>
+    // })
+
+
   return (
     <div>
         <h2>Asteroids ID's</h2>
-        <button onClick={showId} style={{width: "15%", backgroundColor:"pink"}}>click</button>
-        
-        <div>
-           {
-            pick.map((e) => {
-                return(
-                    <li>
-                        {e.id}
-                    </li>
-                )
-            })
-           }
+        <div style={{width: "100%", backgroundColor:"whitesmoke", padding:"5px", display:"flex"}}>
+            <h5>Navbar</h5>
+            <p onClick={() => navigate("/main")} style={{
+                width:"150px", 
+                backgroundColor:"grey", 
+                margin:"auto",
+                padding:"5px", 
+                fontWeight:"700", 
+                borderRadius: "10px",
+                color: "white"
+            }}>
+                From Page
+            </p>
         </div>
+        <button onClick={showId} style={{width: "15%", backgroundColor:"green", borderRadius:"10px"}}>click</button>
+        {/* <div>
+           <ul>{listItems}</ul>
+        </div> */}
 
     </div>
   )
